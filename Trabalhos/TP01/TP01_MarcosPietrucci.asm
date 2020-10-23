8ec5e897abfdda7505c8b410215f3c3d69f8cd89
Marcos Pietrucci <31253602+Marcos-Pietrucci@users.noreply.github.com>
2020-10-23 20:18:05 -0300

    Finalização da parte do processamento

--- Trabalhos/TP01/TP01_MarcosPietrucci.asm
+++ Trabalhos/TP01/TP01_MarcosPietrucci.asm
@@ -28,11 +28,12 @@
 ;----------------------------------------------------------;
             
 org 100h
-
-inicio:     LEA SI,entradas
-main_loop:  CALL le_teclado
+             
+main_loop:  LEA SI,entradas
+            MOV DL, 0
+            CALL le_teclado
             CALL interpreta
-            CALL move_elevador
+mov_func:   CALL move_elevador
             JMP main_loop 
             RET
             
@@ -88,18 +89,20 @@ entry_loop: INC BX
   ;-------- Entrada X --------;          
 continua1:  CMP AH, 78H                 ;78h -> 120dec -> Tecla 'x'
             
-            JE  acabou_luz
+            JNE  nao_acabou
+            JMP  acabou_luz
                       
   ;------- Entrada p ---------;         ;A funcao da entrada p ira aguardar por 'P'
                              
-            CMP AH, 70h                 ;70h -> 112dec -> Tecla 'p'
+nao_acabou: CMP AH, 70h                 ;70h -> 112dec -> Tecla 'p'
             
-           ; JE  para_elev
-  
+            JNE  nao_para
+            CALL para_elev 
+
    
   ;----- Entradas de 'a' a 'h'                    
                              
-            CMP AH, 68h                 ;As entradas de andares (a->h) sao valores de 96->104
+nao_para:   CMP AH, 68h                 ;As entradas de andares (a->h) sao valores de 96->104
             
             JNLE continua2
             CALL manda_andar             ; Foi pedido o elevador em algum andar
@@ -156,12 +159,29 @@ manda_andar PROC
             RET
 manda_andar ENDP
 
+;------------- Para o elevador ------------;
+
+para_elev   PROC
+            PUSHA
+    
+bloqueado:  MOV AH, 0       ;Le a entrada
+            INT 16h                  
+            
+            CMP AL, 50h     ;Caso nao for o 'P', continuar lendo
+            JNZ bloqueado
+            
+            POPA
+            RET    
+para_elev   ENDP
+
 ; -------------- Acabou a luz ------------ ;   
 
 
 acabou_luz  PROC
-            MOV SI, 0
-            MOV CL, 0 
+            LEA SI,entradas
+            MOV CL, 0
+            MOV andar_destino, 1
+            MOV sem_pedidos, 1 
             MOV require_andar[1],0
             MOV require_andar[2],0
             MOV require_andar[3],0
@@ -185,7 +205,9 @@ acabou_luz  PROC
 dec_luz:    CALL print_desce
             DEC AH
             CMP CH,AH
-            JNE  dec_luz
+            JNE dec_luz
+            MOV andar, 1
+            JMP mov_func
 acabou_luz  ENDP     
 
 ; ------------ Move Elevador ------------ ;
@@ -198,7 +220,32 @@ move_elevador     PROC
                   MOV BL,andar
                   MOV AH, require_andar[BX]
                   CMP AH, BL
-                  OR  porta, AH                  ; Caso alguem queira entrar neste andar                                    
+                  OR  porta, AH      ; Caso alguem queira entrar neste andar
+                  
+                  ;Se alguem entrou (porta aberta) remover a solicitacao de entrada
+                  CMP porta, 1
+                  MOV BL, andar
+                  MOV BH, 0
+                  MOV require_andar[BX], 0
+                  
+                  ;Testar caso alguem queira para neste andar
+                  MOV BH,0 
+                  MOV BL,andar
+                  MOV AH, destino_andar[BX]
+                  CMP AH, BL
+                  OR  porta, AH
+                  
+                  ;Se alguem saiu (porta aberta) remover a solicitacao de saida
+                  CMP porta, 1
+                  MOV BL, andar
+                  MOV BH, 0
+                  MOV destino_andar[BX], 0                                  
+                  
+                  ;Testar se o elevador deve ficar ocioso
+                  MOV AH, sem_pedidos
+                  CMP AH, 1
+                  JE  busca_destino_ant  
+                  
                   
                   ;Testar se estou no andar destino
                   MOV AH, andar
@@ -207,10 +254,13 @@ move_elevador     PROC
                   JNE continua3
                   OR porta, 1b                          
                   
+                  MOV BL, AH
+                  MOV BH, 0
+                  MOV destino_andar[BX], 0
                                          
                   ;Caso estiver no destino, devo buscar um novo destino
                   ;Procura destino nos botoes internos
-                  MOV DI, -1
+busca_destino_ant:MOV DI, 0
 busca_destino:    INC DI
                   CMP DI, 9
                   JG  busca_destino2
@@ -231,97 +281,167 @@ loop_destino2:    INC DI
                   
                   
                   ;Nao importa qual a origem do destino, o andar vai ser o DI
-set_destino:      MOV BX, DI
+set_destino:      MOV sem_pedidos, 0
+                  MOV BX, DI
                   MOV BH, 0
                   MOV andar_destino, BL                                 
                   JMP continua3    
                                       
-sem_destino:      MOV andar_destino, 0           ;Se nao ha requisicoes, o elevador vai para o terreo
+sem_destino:      MOV andar_destino, 1           ;Se nao ha requisicoes, o elevador vai para o terreo
+                  MOV sem_pedidos, 1
                                                       
-continua3:        MOV CL, andar
+continua3:        CMP sem_pedidos, 1
+                  JE  ocioso
+continua4:        MOV CL, andar
                   CMP CL, andar_destino
-                  JG  elevador_sobe
-                  JMP elevador_desce         
-     
-              RET    
+                  JL  jmp_sobe  
+                  CALL elevador_desce
+                  JMP fim_move
+                  
+jmp_sobe:         CALL elevador_sobe         
+                  JMP fim_move
+                  
+ocioso:           CMP  andar, 1
+                  JNE  continua4
+                  CALL elevador_parado
+
+fim_move:     RET    
 move_elevador ENDP  
 
 
 ;--------- O elevador vai subir 1 andar ---;
 
 elevador_sobe     PROC
-                  JMP print_sobe
-    
+                  
+                  MOV BL, andar
+                  INC BL
+                  MOV andar, BL
+                  
+                  CALL print_sobe
                   RET
 elevador_sobe     ENDP
                
 ;-------- O elevador vai descer 1 andar ---;
 elevador_desce     PROC
-                   JMP print_desce
+                   MOV BL, andar
+                   DEC BL
+                   MOV andar, BL
+                   CALL print_desce 
+                    
                    RET
 elevador_desce     ENDP
+
+;-------- O elevador fica parado no andar 1 ;
+
+elevador_parado    PROC
+                   CALL print_parado
+                    
+                   RET
+elevador_parado    ENDP
+
+
+
 
 ;-------- Print da MSG de descendo --------;            
 print_desce  PROC
              PUSHA
-             MOV AH, 9h
-             MOV DX, offset descendo
+             MOV AH, 9h 
+             
+             CMP porta, 1
+             JNE continua_desce
+             MOV DX, offset msg_porta_aberta
              INT 21h
-             MOV DX, offset pulalin
+             CALL print_pulalin
+             MOV DX, offset msg_porta_fechada
              INT 21h
-             POPA
-             JMP return
-print_desce  ENDP  
+             MOV porta, 0
+             
+continua_desce:CALL print_pulalin
+               CALL print_pulalin
+               MOV DX, offset descendo
+               INT 21h
+               MOV DX, offset pulalin
+               INT 21h
+                    
+fim_desce:    POPA  
+              RET
+print_desce   ENDP   
                                           
 ;-------- Print da MSG de subindo ---------;                                          
 print_sobe   PROC
              PUSHA
-             MOV AH, 9h
-             MOV DX, offset subindo
+             MOV AH, 9h 
+             
+             CMP porta, 1
+             JNE continua_sobe
+             MOV DX, offset msg_porta_aberta
              INT 21h
-             MOV DX, offset pulalin
+             CALL print_pulalin
+             MOV DX, offset msg_porta_fechada
              INT 21h
-             POPA   
-             JMP return
+             MOV porta, 0
+             
+continua_sobe: CALL print_pulalin
+               CALL print_pulalin
+               MOV DX, offset subindo
+               INT 21h
+               MOV DX, offset pulalin
+               INT 21h
+                    
+fim_sobe:    POPA  
+             RET
 print_sobe   ENDP
-
+ 
+;-------- Print da MSG de parado --------;            
+print_parado PROC
+             PUSHA
+             MOV AH, 9h 
+             CALL print_pulalin
+             CALL print_pulalin
+             MOV DX, offset msg_parado
+             INT 21h
+            
+             POPA  
+             RET
+print_parado ENDP   
+                             
+ 
+ 
+ 
 ;-------- Print do pula linha -------------;
 print_pulalin PROC
-              PUSHA
               MOV AH, 9h
               MOV DX, offset pulalin
-              INT 21h
-              POPA   
-              JMP return
+              INT 21h   
+              RET
 print_pulalin ENDP
 
 
 ;------ Instrucoes de proposito geral ------;
 
- 
-return:     ret
+return: RET 
 
 
 ;Definindo as variaveis
+msg_porta_aberta db "A porta foi aberta$"
+msg_porta_fechada db "A porta foi fechada$"
+msg_parado db "O elevador esta parado no primeiro andar$"
+descendo db "O elevador desceu 1 andar$"
+subindo  db "O elevador subiu 1 andar$"
+pulalin db 0Dh,0Ah,'$'
 
 ; FLAGS do elevador
 
 terreo  DB 0
 topo    DB 0
 porta   DB 0    ;0 para fechada, 1 para aberta
-direcao DB 0    ;0 para subindo, 1 para descendo
 
-andar_destino DB 0
+andar_destino DB 1     ;Inicialmente o elevador comeca sem pedidos
+sem_pedidos   DB 1
                        
-andar   DB 0
+andar   DB 1
 
-require_andar DB 0, 0, 0, 0, 0, 0, 0, 0     ;Botoes externos
-destino_andar DB 0, 0, 0, 0, 0, 0, 0, 0     ;Botoes internos 
-entradas db 0                                                
+require_andar DB 0, 0, 0, 0, 0, 0, 0, 0, 0     ;Botoes externos
+destino_andar DB 0, 0, 0, 0, 0, 0, 0, 0, 0     ;Botoes internos 
 
-
-descendo db "O elevador desceu 1 andar$"
-subindo  db "O elevador subiu 1 andar$"
-pulalin db 0Dh,0Ah,'$'
-
-
+entradas db 0 
